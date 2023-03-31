4bcb35a55e417904ee0de8cef1bcec9189422b56
longsiru <sirulong89@gmail.com>
2023-04-01 00:13:33 +0900

    detail+404

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return "what are u doing here?"; //输入没有定义的url的时候出现的画面
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    //router.push(`/movies/${id}`); 只出现id
+    //出现id并出现检索的详细，但是可能很多人不想看到详细，那么我们就要把这些隐藏起来, 用as。url看不见了，但是inspect里面是可以查看的
+    router.push(`/movies/${title}/${id}`);
+  };
   return (
     <div>
       <div className="container">
         <Seo title="Home" />
         {results?.map((movie) => (
-          <div key={movie.id}>
-            <div className="movie" key={movie.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              />
-              <h4>{movie.original_title}</h4>
-            </div>
+          <div
+            onClick={() => onClick(movie.id, movie.original_title)}
+            className="movie"
+            key={movie.id}
+          >
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+
+            <h4>
+              <Link
+                legacyBehavior
+                href={`/movies/${movie.original_title}/${movie.id}`}
+              >
+                <a>{movie.original_title}</a>
+              </Link>
+            </h4>
           </div>
         ))}
         <style jsx>{`
@@ -22,6 +37,9 @@ export default function Home({ results }) {
             grid-template-columns: 1fr 1fr;
             padding: 20px;
             gap: 20px;
+          }
+          .movie {
+            cursor: pointer;
           }
           .movie img {
             max-width: 100%;
